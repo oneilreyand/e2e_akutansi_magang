6ed43e53a9dfd03d8adb2599f0cd60c2c22ea380
Zve <154986820+Diocta28@users.noreply.github.com>
2024-12-20 10:38:46 +0700

    Update test.cy.js

--- cypress/e2e/test/test.cy.js
+++ cypress/e2e/test/test.cy.js
@@ -1,5 +1,5 @@
 describe('test website cashflow assist id bagian login', () => {
-    before(() => {
+    beforeEach(() => {
         cy.visit('https://cashflow.assist.id/')
     })
     // test case 1
@@ -76,7 +76,72 @@ describe('test website cashflow assist id bagian login', () => {
             .and('have.value', 'password')
     })
     // test case 7
-    it.only('mencoba menekan checkbox remember me', () => {
-        cy.get('.PrivateSwitchBase-input').click()
+    it('mencoba menekan checkbox remember me', () => {
+        cy.get('.PrivateSwitchBase-input')
+            .click({ force: true })
+    })
+    // test case 8
+    it('memeriksa placeholder bagian alamat email', () => {
+        cy.get('input[type="email"]')
+            .should('be.visible')
+            .should('have.attr', 'placeholder', "Alamat email")
+    })
+    // test case 9
+    it('memeriksa placeholder bagian password', () => {
+        cy.get('input[type="password"]')
+            .should('be.visible')
+            .should('have.attr', 'placeholder', "Masukkan password")
+    })
+    // test case 10
+    it('memeriksa form apakah tersedia atau tidak', () => {
+        cy.get('form')
+            .should('exist')
+    })
+    // test case 11
+    it('memeriksa apakah label dan kolom username tersedia', () => {
+        cy.get('label')
+            .contains("Alamat email")
+        cy.get('input[type="email"]')
+            .should('exist')
+    })
+    // test case 12
+    it('memeriksa apakah label dan kolom password tersedia', () => {
+        cy.get('label')
+            .contains("Password")
+        cy.get('input[type="password"]')
+            .should('exist')
+    })
+    // test case 13
+    it('memeriksa halaman lupa password', () => {
+        cy.get('[data-testid="login-forgot-password"]')
+            .should('be.visible')
+            .click()
+        cy.url()
+            .should('include', 'https://cashflow.assist.id/auth/forgot-password')
+    })
+    // test case 14
+    it('memeriksa apakah button login tersedia', () => {
+        cy.get('[data-testid="login-submit-button"]')
+            .should('be.visible')
+            .invoke('text')
+            .should('eq', 'Masuk')
+    })
+    // test case 15
+    it('memeriksa apakah tombol login dengan google tersedia', () => {
+        cy.get('[data-testid="login-google-button"]')
+            .should('be.visible')
+            .invoke('text')
+            .should('eq', 'Masuk dengan Google')
+        cy.get('[data-testid="login-google-button"]')
+            .click()
+    })
+    // test case 16
+    it.only('memeriksa bagian register', () => {
+        cy.get('[data-testid="login-register-button"]')
+            .should('be.visible')
+            .invoke('text')
+            .should('eq', 'Daftar sekarang')
+        cy.get('[data-testid="login-register-button"]')
+            .click()
     })
 })
